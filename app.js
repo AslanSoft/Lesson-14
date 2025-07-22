b388fa65d703845a8b7bf7340365d58c4323fcdd
AslanSoft <beggyplayy@gmail.com>
2025-07-22 20:07:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+// Bular backend dan malumot olish, yaratish, o'chiris xamda o'zgartirish uchun ishlaydi
+
+// c ----> create ----> post bu beckendga zapros yuborish malumot yaratish
+// r ----> read -----> get bu beckenddan malumot olib kelish 
+// u ----> update ----> put, patch bu malumotni o'zgartirish
+// d ----> delete ----> bu malumotlarni o'chirish
+
+// bytes b ---> "64" yani ma'lumotlarni ascii jadvalida qaytaradi yani shifrlab qaytaradi
+// blob malumotlarni {  } array formatida qaytaradi 
+// formdata rasm, videolarni formdataga o'tqizib beradi 
+// json {"salom": "1"} json bu keyni xam valueni xam string o'tqizadi
+// json.stringify bu oddiy obyektni string tipiga o'tqizadi 
+// json,parse bu stringify dagi malumotlarni yechib beradi
